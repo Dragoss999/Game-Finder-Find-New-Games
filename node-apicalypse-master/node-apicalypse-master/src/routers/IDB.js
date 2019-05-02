c194853432ed1574bcf0579d15e683b5390d2810
Dragoss999 <43641638+Dragoss999@users.noreply.github.com>
2019-05-02 09:55:38 +0100

    Comments

--- node-apicalypse-master/node-apicalypse-master/src/routers/IDB.js
+++ node-apicalypse-master/node-apicalypse-master/src/routers/IDB.js
@@ -31,4 +31,35 @@ router.get('/api/genres/search/:name', async (req,res) => {
     }
 });
 
+/*
+This is what I tried to do to get the games showing up but I couldn't seem to get it working
+router.get('/api/games', async (req,res) => {
+    try{
+        const response = await apicalypse(requestOptions)
+        .fields(['name','url', 'genres'])
+        .where('genre'= 31)
+        .request('/games');
+        if(response.data.length === 0){
+            return res.send({'error' : 'Genre not found with that name'});
+        }
+        res.send(response.data);
+    }    
+    catch(error) {
+        console.log(error);
+        res.status(400).send();
+    }
+});
+
+Genre IDs:
+Adventure ID = 31
+Fighting ID = 4
+Music ID = 7
+Platform ID = 8
+Puzzle ID = 9
+Racing ID = 10
+Shooter ID = 5
+Sport ID = 14
+Strategy ID = 15
+*/
+
 module.exports = router;
